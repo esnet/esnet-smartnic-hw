690ca76e3454e45b7081faaaa5d79d61b2cad053
Peter Bengough <pbengough@es.net>
2022-04-07 08:00:50 -0700

    (cfg:paths) Updated paths and part.tcl to retrieve Xilinx board files from the open-nic-shell submodule.

--- cfg/part.tcl
+++ cfg/part.tcl
@@ -1,3 +1,7 @@
+# Set path to board files
+set ons_root $env(ONS_ROOT)
+set_param board.repoPaths $ons_root/board_files/Xilinx
+
 # Target part/board
 set PART xcu280-fsvh2892-2L-e
 set BOARD_PART xilinx.com:au280:part0:1.1
