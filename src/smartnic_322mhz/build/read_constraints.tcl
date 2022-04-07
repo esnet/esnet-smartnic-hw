67d41b4573c5c71269917c10555ee5842eff61d7
Peter Bengough <pbengough@es.net>
2022-04-07 08:18:33 -0700

    (smartnic_322mhz:build) Re-worked smartnic_322mhz build scripts to use the standardized build scripts from esnet-fpga-library.

--- src/smartnic_322mhz/build/read_constraints.tcl
+++ src/smartnic_322mhz/build/read_constraints.tcl
@@ -1,11 +1,5 @@
-set proj_root $env(PROJ_ROOT)
+read_xdc constraints/timing.xdc
+read_xdc constraints/place.xdc
+
 set lib_root $env(LIB_ROOT)
-set app_root $env(APP_ROOT)
-
-read_xdc $proj_root/src/smartnic_322mhz/build/constraints/timing.xdc
-read_xdc $proj_root/src/smartnic_322mhz/build/constraints/place.xdc
-
-read_xdc -unmanaged $lib_root/src/sync/build/sync.xdc
-
-if { [file exists $app_root/app_if/timing.xdc ] } {read_xdc $app_root/app_if/timing.xdc }
-if { [file exists $app_root/app_if/place.xdc  ] } {read_xdc $app_root/app_if/place.xdc  }
+read_xdc $lib_root/src/sync/build/sync.xdc
