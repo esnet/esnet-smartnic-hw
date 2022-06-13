0dab0ad43d5c840314d189f617b456e023c80157
Jonathan Sewter <jsewter@es.net>
2022-06-13 17:30:35 -0400

    p4_example: update xilinx IP and testbench for 2022.1.

--- examples/unsupported/p4_example/tb/include/tb_env.svh
+++ examples/unsupported/p4_example/tb/include/tb_env.svh
@@ -71,6 +71,9 @@ class tb_env extends std_verif_pkg::base;
     // Pointer to SDNet driver
     protected chandle _drv;
 
+    // Pointer to Table context vector
+    protected chandle _ctxPtr[$];
+
     // Timestamp
     virtual timestamp_if #() timestamp_vif;
 
@@ -204,7 +207,7 @@ class tb_env extends std_verif_pkg::base;
         import sdnet_0_pkg::*;
         debug_msg("---------------- SDnet: Create. -------------");
         if (this._drv == null) begin
-            this._drv = XilVitisNetP4DpiGetEnv(hier_path);
+            this._drv = XilVitisNetP4DpiCreateEnv(hier_path);
             debug_msg("---------------- SDnet: Driver create done. -------------");
         end else begin
             debug_msg("---------------- SDnet: Driver already exists. -------------");
@@ -216,35 +219,9 @@ class tb_env extends std_verif_pkg::base;
     task sdnet_init();
         import sdnet_0_pkg::*;
 
-        chandle env = this._drv;
-
-        debug_msg("---------------- SDnet: Init Tables. -------------");
-        //
-        // Table initialization routine as captured in sdnet_0_pkg::intialize()
-        //
-        //   The stock sdnet_0_pkg::initialize() task also initializes the environment, with a
-        //   call to XilVitisNetP4DpiGetEnv(). That function can only be called once per sim run, which
-        //   makes it impossible to initialize tables more than once (useful for multiple testcases
-        //   within a single run, or within a regression run).
-        //
-        if (env != null) begin
-            for (int tbl_idx = 0; tbl_idx < XilVitisNetP4TableList.size(); tbl_idx++) begin
-                case (XilVitisNetP4TableList[tbl_idx].Config.Mode)
-                    XIL_VITIS_NET_P4_TABLE_MODE_BCAM : begin
-                        XilVitisNetP4BcamInit(XilVitisNetP4TableList[tbl_idx].PrivateCtxPtr, env, XilVitisNetP4TableList[tbl_idx].Config.CamConfig);
-                    end
-                    XIL_VITIS_NET_P4_TABLE_MODE_TCAM : begin
-                        XilVitisNetP4TcamInit(XilVitisNetP4TableList[tbl_idx].PrivateCtxPtr, env, XilVitisNetP4TableList[tbl_idx].Config.CamConfig);
-                    end
-                    XIL_VITIS_NET_P4_TABLE_MODE_STCAM : begin
-                        XilVitisNetP4StcamInit(XilVitisNetP4TableList[tbl_idx].PrivateCtxPtr, env, XilVitisNetP4TableList[tbl_idx].Config.CamConfig);
-                    end
-                    XIL_VITIS_NET_P4_TABLE_MODE_DCAM : begin
-                        XilVitisNetP4DcamInit(XilVitisNetP4TableList[tbl_idx].PrivateCtxPtr, env, XilVitisNetP4TableList[tbl_idx].Config.CamConfig);
-                    end
-                endcase
-            end
-        end
+        debug_msg("---------------- SDnet: Init tables. -------------");
+        initialize(this._ctxPtr, this._drv);
+        debug_msg("---------------- SDnet: Init tables done.. -------------");
     endtask
 
     // Clean up SDNet tables
@@ -253,212 +230,100 @@ class tb_env extends std_verif_pkg::base;
         import sdnet_0_pkg::*;
 
         debug_msg("---------------- SDnet: Destroy Tables. -------------");
-        terminate();
+        terminate(this._ctxPtr);
     endtask
 
-    // sdnet_table_init is a copy of the initial block in the example_control.sv file of xilinx sdnet_0 example design
+    // sdnet_table_init is based on the procedure described in the example_control.sv file of xilinx sdnet_0 example design
     task sdnet_table_init_from_file(input string filename);
         import example_design_pkg::*;
         const bit VERBOSE = (this.get_debug_level() > 1);
 
-        strArray table_entry_handles[string][$];
-        strArray empty_strArray_list [$];
-        strArray empty_strArray;
-        strArray match_fields;
+        CliCmdStruct cli_cmds[$];
+        CliCmdStruct cli_cmd;
+
+        string table_format_str;
         strArray action_params;
-        strArray cmd_line;
-        string str_line;
-        string command;
-        string table_name;
-        string action_name;
-        int fh, delim_idx;
-        int entry_priority;
-        int entry_handle;
         bitArray key, mask;
         bitArray response;
+        int table_is_ternary;
+        int action_id;
+        int action_id_width;
+        int entry_priority;
+        int action_arg_widths[$];
 
-        /*------------------
-        traffic_start <= 0;
+        chandle CtxPtr[$] = this._ctxPtr;
 
-        // Wait for reset signal to start
-        @ (posedge axi_aresetn);
-        for (int d=0; d<20; d++) @ (posedge axi_aclk);
+        sdnet_init();
 
-        // instantiate drivers
-        sdnet_0_pkg::initialize("example_top.example_control");
-        ------------------*/
+        // Parse CLI command file (e.g. config.txt)
+        parse_cli_commands(filename, cli_cmds);
 
-        info_msg("Opening CLI command file...");
+        for (int cmd_idx=0; cmd_idx<cli_cmds.size(); cmd_idx++) begin
+           cli_cmd = cli_cmds[cmd_idx];
+           case (cli_cmd.cmd_op)
 
-        // open CLI commands file
-        fh = $fopen(filename, "r");
-        if (!fh) begin
-            $fatal(1, "** Error: Failed to open file './cli_commands.txt' file");
-        end
+               TBL_ADD: begin
+                   table_format_str = sdnet_0_pkg::get_table_format_string(cli_cmd.table_name);
+                   table_is_ternary = sdnet_0_pkg::table_is_ternary(cli_cmd.table_name);
+                   action_id        = sdnet_0_pkg::get_action_id(cli_cmd.table_name, cli_cmd.action_name);
+                   action_id_width  = sdnet_0_pkg::get_table_action_id_width(cli_cmd.table_name);
+                   sdnet_0_pkg::get_action_arg_widths(cli_cmd.table_name, cli_cmd.action_name, action_arg_widths);
+                   parse_match_fields(table_format_str, cli_cmd.match_fields, key, mask);
+                   split_action_params_and_prio(table_is_ternary, cli_cmd.action_params, action_params, entry_priority);
+                   parse_action_parameters(action_arg_widths, action_id, action_id_width, action_params, response);
+                   if (VERBOSE) begin
+                     $display("** Info: Adding entry to table %0s", cli_cmd.table_name);
+                     $display("  - acion:\t%0s", cli_cmd.action_name);
+                     $display("  - match key:\t0x%0x", key);
+                     $display("  - key mask:\t0x%0x", mask);
+                     $display("  - response:\t0x%0x", response);
+                     $display("  - priority:\t%0d", entry_priority);
+                   end
+                   sdnet_0_pkg::table_add(CtxPtr, cli_cmd.table_name, key, mask, response, entry_priority);
+                   if (VERBOSE) $display("** Info: Entry has been added with handle %0d", cli_cmd.entry_id);
+               end
 
-        // read file lines
-        while(!$feof(fh)) begin
-            if($fgets(str_line, fh)) begin
+               TBL_MODIFY : begin
+                   action_id        = sdnet_0_pkg::get_action_id(cli_cmd.table_name, cli_cmd.action_name);
+                   action_id_width  = sdnet_0_pkg::get_table_action_id_width(cli_cmd.table_name);
+                   table_format_str = sdnet_0_pkg::get_table_format_string(cli_cmd.table_name);
+                   sdnet_0_pkg::get_action_arg_widths(cli_cmd.table_name, cli_cmd.action_name, action_arg_widths);
+                   parse_action_parameters(action_arg_widths, action_id, action_id_width, cli_cmd.action_params, response);
+                   parse_match_fields(table_format_str, cli_cmd.match_fields, key, mask);
+                   if (VERBOSE) begin
+                     $display("** Info: Modifying entry from table %0s", cli_cmd.table_name);
+                     $display("  - acion:\t%0s", cli_cmd.action_name);
+                     $display("  - response:\t0x%0x", response);
+                   end
+                   sdnet_0_pkg::table_modify(CtxPtr, cli_cmd.table_name, key, mask, response);
+                   if (VERBOSE) $display("** Info: Entry has been modified with handle %0d", cli_cmd.entry_id);
+               end
 
-                // remove '\n' at the end of line
-                while (str_line[str_line.len()-1] == "\n")
-                    str_line = str_line.substr(0, str_line.len()-2);
+               TBL_DELETE : begin
+                   table_format_str = sdnet_0_pkg::get_table_format_string(cli_cmd.table_name);
+                   parse_match_fields(table_format_str, cli_cmd.match_fields, key, mask);
+                   if (VERBOSE) begin
+                     $display("** Info: Deleting entry from table %0s", cli_cmd.table_name);
+                     $display("  - match key:\t0x%0x", key);
+                     $display("  - key mask:\t0x%0x", mask);
+                   end
+                   sdnet_0_pkg::table_delete(CtxPtr, cli_cmd.table_name, key, mask);
+                   if (VERBOSE) $display("** Info: Entry has been deleted with handle %0d", cli_cmd.entry_id);
+               end
 
-                // ignore comments and empty lines
-                if (str_line[0] == "%"  ||
-                    str_line[0] == "#"  ||
-                    str_line[0] == "\n" ||
-                    str_line.len() == 0)
-                    continue;
+               TBL_CLEAR : begin
+                   if (VERBOSE) $display("** Info: Deleting all entries from table %0s", cli_cmd.table_name);
+                   sdnet_0_pkg::table_clear(CtxPtr, cli_cmd.table_name);
+               end
 
-                // split line and parse command
-                cmd_line = split(str_line, " ");
-                command = cmd_line[0];
-                case (command)
+               RST_STATE : begin
+                   if (VERBOSE) $display("** Info: Reseting VitisNet IP instance to default state");
+                   sdnet_0_pkg::reset_state(CtxPtr);
+               end
 
-                     // table_add <table name> <action name> <match fields> => [action parameters] [priority]
-                     "table_add" : begin
-                         // parse args
-                         for (delim_idx = 1; delim_idx < cmd_line.size(); delim_idx++) begin
-                             if (cmd_line[delim_idx] == "=>")
-                                 break;
-                         end
-                         table_name   = cmd_line[1];
-                         action_name  = cmd_line[2];
-                         match_fields = cmd_line[3:delim_idx-1];
-                         parse_match_fields(table_name, match_fields, key, mask);
-                         split_action_params_and_prio(table_name, cmd_line[delim_idx+1:cmd_line.size()-1], action_params, entry_priority);
-                         parse_action_parameters(table_name, action_name, action_params, response);
-                         // execute command
-                         if (VERBOSE) begin
-                           $display("** Info: Adding entry to table %0s", table_name);
-                           $display("  - match key:\t0x%0x", key);
-                           $display("  - key mask:\t0x%0x", mask);
-                           $display("  - response:\t0x%0x", response);
-                           $display("  - priority:\t%0d", entry_priority);
-                         end
-                         sdnet_0_pkg::table_add(table_name, key, mask, response, entry_priority);
-                         // create entry handle
-                         if (!table_entry_handles.exists(table_name))
-                             table_entry_handles[table_name] = empty_strArray_list;
-                         for (int i = 0; i <= table_entry_handles[table_name].size(); i++) begin
-                             if (i == table_entry_handles[table_name].size())
-                                table_entry_handles[table_name][i] = empty_strArray;
-                             if (table_entry_handles[table_name][i].size() == 0) begin
-                                 table_entry_handles[table_name][i] = match_fields;
-                                 entry_handle = i;
-                                 break;
-                             end
-                         end
-                         if (VERBOSE)
-                           $display("** Info: Entry has been added with handle %0d", entry_handle);
-                     end
+           endcase
+       end
 
-                     // table_modify <table name> <action name> <entry handle> [action parameters]
-                     "table_modify" : begin
-                         // parse args
-                         table_name    = cmd_line[1];
-                         action_name   = cmd_line[2];
-                         entry_handle  = cmd_line[3].atoi();
-                         action_params = cmd_line[4:cmd_line.size()-1];
-                         parse_action_parameters(table_name, action_name, action_params, response);
-                         // get entry handle
-                         if (!table_entry_handles.exists(table_name)) begin
-                             $fatal(1, "** Error: Table entry '%0d' not found for table '%0s'", entry_handle, table_name);
-                         end
-                         if (table_entry_handles[table_name][entry_handle].size() == 0) begin
-                             $fatal(1, "** Error: Table entry '%0d' not found for table '%0s'", entry_handle, table_name);
-                         end
-                         match_fields = table_entry_handles[table_name][entry_handle];
-                         parse_match_fields(table_name, match_fields, key, mask);
-                         // execute command
-                         if (VERBOSE) begin
-                           $display("** Info: Modifying entry from table %0s", table_name);
-                           $display("  - response:\t0x%0x", response);
-                         end
-                         sdnet_0_pkg::table_modify(table_name, key, mask, response);
-                         if (VERBOSE)
-                           $display("** Info: Entry has been modified with handle %0d", entry_handle);
-                     end
-
-                     // table_delete <table name> <entry handle>
-                     "table_delete" : begin
-                         // parse args
-                         table_name   = cmd_line[1];
-                         entry_handle = cmd_line[2].atoi();
-                         // get entry handle
-                         if (!table_entry_handles.exists(table_name)) begin
-                             $fatal(1, "** Error: Table entry '%0d' not found for table '%0s'", entry_handle, table_name);
-                         end
-                         if (table_entry_handles[table_name][entry_handle].size() == 0) begin
-                             $fatal(1, "** Error: Table entry '%0d' not found for table '%0s'", entry_handle, table_name);
-                         end
-                         match_fields = table_entry_handles[table_name][entry_handle];
-                         parse_match_fields(table_name, match_fields, key, mask);
-                         // execute command
-                         if (VERBOSE) begin
-                           $display("** Info: Deleting entry from table %0s", table_name);
-                           $display("  - match key:\t0x%0x", key);
-                           $display("  - key mask:\t0x%0x", mask);
-                         end
-                         sdnet_0_pkg::table_delete(table_name, key, mask);
-                         if (VERBOSE)
-                           $display("** Info: Entry has been deleted with handle %0d", entry_handle);
-                         // delete entry handle
-                         table_entry_handles[table_name][entry_handle] = empty_strArray;
-                     end
-
-                     // table_clear <table name>
-                     "table_clear" : begin
-                         table_name = cmd_line[1];
-                         for (int i = 0; i <= table_entry_handles[table_name].size(); i++) begin
-                             if (table_entry_handles[table_name][entry_handle].size() > 0) begin
-                                 match_fields = table_entry_handles[table_name][entry_handle];
-                                 parse_match_fields(table_name, match_fields, key, mask);
-                                 sdnet_0_pkg::table_delete(table_name, key, mask);
-                             end
-                         end
-                     end
-
-                     // reset_state
-                     "reset_state" : begin
-                         $display("** Info: Reseting 'sdnet_0' to default state");
-                         sdnet_0_pkg::reset_state();
-                     end
-
-                     // run_traffic <file name>
-                     "run_traffic" : begin
-                         /*------------------
-                         // wait some time for simulation to settle down before running traffic
-                         for (int d=0; d<100; d++) @ (posedge axi_aclk);
-                         // trigger stimulus generation
-                         $display("** Info: Running traffic from file '%0s'", cmd_line[1]);
-                         traffic_filename = cmd_line[1];
-                         //traffic_start    <= 1;
-                         @ (posedge axi_aclk);
-                         //traffic_start    <= 0;
-                         // wait for stimulus generator and checker to be done
-                         @ (posedge (stimulus_done | checker_done));
-                         @ (posedge (checker_done  | stimulus_done));
-                         @ (posedge axi_aclk);
-                         ------------------*/
-                         break;
-                     end
-
-                     // exit
-                     "exit" : begin
-                         break;
-                     end
-
-                     // ignore invalid commands
-                     default : begin
-                         $display("** Info: Ignoring invalid command '%0s'", command);
-                         continue;
-                     end
-
-                endcase
-            end
-        end
   endtask
 
 endclass : tb_env
