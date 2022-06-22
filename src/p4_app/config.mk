e42569a3d651c6ea1f38983956a9ab0b86342241
Jonathan Sewter <jsewter@es.net>
2022-06-22 08:56:46 -0400

    p4_app (reg): re-root regio include to LIB_ROOT.

--- src/p4_app/config.mk
+++ src/p4_app/config.mk
@@ -3,7 +3,7 @@
 # -----------------------------------------------
 # Set relative to IP directory
 # Note: IP_ROOT is configured in calling (parent) Makefile
-export PROJ_ROOT ?= $(abspath $(IP_ROOT)/../..)
+export PROJ_ROOT = $(abspath $(IP_ROOT)/../..)
 
 # All other project paths can be derived
 include $(PROJ_ROOT)/paths.mk
@@ -14,9 +14,6 @@ include $(PROJ_ROOT)/paths.mk
 # IP library name - if unset, defaults to IP_ROOT directory name (with ".HDL" suffix stripped, when present)
 IP_NAME =
 
-REGIO_YAML_INC_DIR = $(IP_ROOT)
-
-export P4_FILE ?= $(abspath $(IP_ROOT)/p4/$(IP_NAME).p4)
 
 # -----------------------------------------------
 # Import base IP config
