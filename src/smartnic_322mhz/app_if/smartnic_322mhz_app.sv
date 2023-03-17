6ba00048807056eea4a126c61572e0daa73c17c1
Jonathan Sewter <jsewter@es.net>
2023-03-17 17:03:27 -0400

    Remove copyright headers from individual source files.

--- src/smartnic_322mhz/app_if/smartnic_322mhz_app.sv
+++ src/smartnic_322mhz/app_if/smartnic_322mhz_app.sv
@@ -1,20 +1,3 @@
-// =============================================================================
-//  NOTICE: This computer software was prepared by The Regents of the
-//  University of California through Lawrence Berkeley National Laboratory
-//  and Peter Bengough hereinafter the Contractor, under Contract No.
-//  DE-AC02-05CH11231 with the Department of Energy (DOE). All rights in the
-//  computer software are reserved by DOE on behalf of the United States
-//  Government and the Contractor as provided in the Contract. You are
-//  authorized to use this computer software for Governmental purposes but it
-//  is not to be released or distributed to the public.
-//
-//  NEITHER THE GOVERNMENT NOR THE CONTRACTOR MAKES ANY WARRANTY, EXPRESS OR
-//  IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.
-//
-//  This notice including this sentence must appear on any copies of this
-//  computer software.
-// =============================================================================
-
 // smartnic_322mhz_app (empty) stub module. Used for platform level tests.
 module smartnic_322mhz_app
 #(
