1fd2daa2df42a0f4e9457bda812ecd7de1070ed9
Peter Bengough <pbengough@es.net>
2023-11-07 12:50:31 -0500

    (examples:p2p) Adds connections to 'smartnic_322mhz_app' interface for truncation control.

--- examples/p2p/src/p2p/rtl/src/p2p_pkg.sv
+++ examples/p2p/src/p2p/rtl/src/p2p_pkg.sv
@@ -12,4 +12,40 @@ package p2p_pkg;
     // Typedefs
     // --------------------------------------------------------------
 
+    // Typedefs from smartnic_322mhz_pkg --- begin
+    typedef enum logic [1:0] {
+        CMAC_PORT0 = 2'h0,
+        CMAC_PORT1 = 2'h1,
+        HOST_PORT0 = 2'h2,
+        HOST_PORT1 = 2'h3
+    } port_encoding_t;
+
+    typedef union packed {
+        port_encoding_t encoded;
+        bit [1:0]       raw;
+    } port_t;
+
+    typedef enum logic [2:0] {
+        CMAC0 = 3'h0,
+        CMAC1 = 3'h1,
+        HOST0 = 3'h2,
+        HOST1 = 3'h3,
+        LOOPBACK = 3'h7
+    } egr_tdest_encoding_t;
+
+    typedef union packed {
+        egr_tdest_encoding_t encoded;
+        bit [2:0]       raw;
+    } egr_tdest_t;
+
+    typedef struct packed {
+        logic [15:0] pid;
+        logic        trunc_enable;
+        logic [15:0] trunc_length;
+        logic        rss_enable;
+        logic [11:0] rss_entropy;
+        logic        hdr_tlast;
+    } tuser_smartnic_meta_t;
+    // Typedefs from smartnic_322mhz_pkg --- end
+
 endpackage : p2p_pkg
