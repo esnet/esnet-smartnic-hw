2cc0206c98e70500c9ee7e14f075dbca7bfcbc78
Jonathan Sewter <jsewter@es.net>
2024-01-15 15:55:05 -0500

    Remove copyright notice.

--- src/smartnic_322mhz/rtl/src/smartnic_322mhz_hbm.sv
+++ src/smartnic_322mhz/rtl/src/smartnic_322mhz_hbm.sv
@@ -1,20 +1,3 @@
-// =============================================================================
-//  Notice: This computer software was prepared by The Regents of the
-//  University of California through Lawrence Berkeley National Laboratory
-//  and Jonathan Sewter hereinafter the Contractor, under Contract No.
-//  DE-AC02-05CH11231 with the Department of Energy (DOE). All rights in the
-//  computer software are reserved by DOE on behalf of the United States
-//  Government and the Contractor as provided in the Contract. You are
-//  authorized to use this computer software for Governmental purposes but it
-//  is not to be released or distributed to the public.
-//
-//  NEITHER THE GOVERNMENT NOR THE CONTRACTOR MAKES ANY WARRANTY, EXPRESS OR
-//  IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.
-//
-//  This notice including this sentence must appear on any copies of this
-//  computer software.
-// =============================================================================
-
 module smartnic_322mhz_hbm #(
     parameter bit HBM_STACK = 1'b0// 0: Left stack (4GB); 1: Right stack (4GB)
 ) (
