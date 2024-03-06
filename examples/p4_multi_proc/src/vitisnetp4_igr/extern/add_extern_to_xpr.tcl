2d5fc3dcd7445d54cc7e091252ceed742ebf20eb
Peter Bengough <pbengough@es.net>
2024-03-05 22:20:34 -0500

    (p4_multi_proc) Initial commit of example design with multiple p4 processors.

---
+++ examples/p4_multi_proc/src/vitisnetp4_igr/extern/add_extern_to_xpr.tcl
@@ -0,0 +1,3 @@
+open_project ../example/sdnet_0_ex/sdnet_0_ex.xpr
+import_files ./rtl
+close_project
---
+++ examples/p4_multi_proc/src/vitisnetp4_igr/verif/src/vitisnetp4_igr_verif_pkg.sv
@@ -0,0 +1,6 @@
+package vitisnetp4_igr_verif_pkg;
+
+   `include "vitisnetp4_igr_agent.svh"
+
+endpackage : vitisnetp4_igr_verif_pkg
+
