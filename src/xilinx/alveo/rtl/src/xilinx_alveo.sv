348eab30f518b426e8071d7a4e697fffbdceed02
Jonathan Sewter <jsewter@es.net>
2025-02-26 12:16:02 -0500

    xilinx.alveo: decouple block-level and top-level CMAC/QDMA AXI-S metadata signaling.

--- src/xilinx/alveo/rtl/src/xilinx_alveo.sv
+++ src/xilinx/alveo/rtl/src/xilinx_alveo.sv
@@ -54,16 +54,37 @@ module xilinx_alveo
     // =========================================================================
     axi4l_intf #() axil_syscfg ();
     axi4l_intf #() axil_qdma ();
-    axi4l_intf #() axil_cmac [NUM_CMAC_REGMAP] ();
+    axi4l_intf #() axil_cmac [NUM_CMAC] ();
     axi4l_intf #() axil_sysmon ();
     axi4l_intf #() axil_qspi ();
-
-    axi4s_intf #(.DATA_BYTE_WID(DMA_ST_DATA_BYTE_WID), .TID_T(dma_st_qid_t), .TUSER_T(dma_st_h2c_axis_tuser_t)) __axis_h2c ();
-    axi4s_intf #(.DATA_BYTE_WID(DMA_ST_DATA_BYTE_WID), .TID_T(dma_st_qid_t), .TUSER_T(dma_st_c2h_axis_tuser_t)) __axis_c2h ();
 
     // =========================================================================
     // Host
     // =========================================================================
+    // (Local) interfaces
+    axi4s_intf #(.DATA_BYTE_WID(xilinx_qdma_pkg::AXIS_DATA_BYTE_WID), .TID_T(xilinx_qdma_pkg::axis_tid_t), .TDEST_T(xilinx_qdma_pkg::axis_tdest_t), .TUSER_T(xilinx_qdma_pkg::axis_tuser_t)) __axis_h2c ();
+    axi4s_intf #(.DATA_BYTE_WID(xilinx_qdma_pkg::AXIS_DATA_BYTE_WID), .TID_T(xilinx_qdma_pkg::axis_tid_t), .TDEST_T(xilinx_qdma_pkg::axis_tdest_t), .TUSER_T(xilinx_qdma_pkg::axis_tuser_t)) __axis_h2c__async ();
+    axi4s_intf #(.DATA_BYTE_WID(xilinx_qdma_pkg::AXIS_DATA_BYTE_WID), .TID_T(xilinx_qdma_pkg::axis_tid_t), .TDEST_T(xilinx_qdma_pkg::axis_tdest_t), .TUSER_T(xilinx_qdma_pkg::axis_tuser_t)) __axis_c2h__async ();
+    axi4s_intf #(.DATA_BYTE_WID(xilinx_qdma_pkg::AXIS_DATA_BYTE_WID), .TID_T(xilinx_qdma_pkg::axis_tid_t), .TDEST_T(xilinx_qdma_pkg::axis_tdest_t), .TUSER_T(xilinx_qdma_pkg::axis_tuser_t)) __axis_c2h ();
+
+    // (Local) signals
+    xilinx_qdma_pkg::axis_tid_t   __axis_h2c_tid;
+    xilinx_qdma_pkg::axis_tdest_t __axis_h2c_tdest;
+    xilinx_qdma_pkg::axis_tuser_t __axis_h2c_tuser;
+
+    dma_st_axis_tid_t   axis_h2c_tid;
+    dma_st_axis_tdest_t axis_h2c_tdest;
+    dma_st_axis_tuser_t axis_h2c_tuser;
+
+    dma_st_axis_tid_t   axis_c2h_tid;
+    dma_st_axis_tdest_t axis_c2h_tdest;
+    dma_st_axis_tuser_t axis_c2h_tuser;
+
+    xilinx_qdma_pkg::axis_tid_t   __axis_c2h_tid;
+    xilinx_qdma_pkg::axis_tdest_t __axis_c2h_tdest;
+    xilinx_qdma_pkg::axis_tuser_t __axis_c2h_tuser;
+
+    // Host instantiation
     xilinx_alveo_host #(
         .PCIE_LINK_WID ( alveo_hw_if.PCIE_LINK_WID )
     ) i_xilinx_alveo_host (
@@ -82,9 +103,6 @@ module xilinx_alveo
         .axis_c2h      ( __axis_c2h )
     );
 
-    // (Local) interfaces
-    axi4s_intf #(.DATA_BYTE_WID(DMA_ST_DATA_BYTE_WID), .TID_T(dma_st_qid_t), .TUSER_T(dma_st_h2c_axis_tuser_t)) __axis_h2c__async ();
-
     // AXI-S CDC
     axi4s_fifo_async #(
         .DEPTH        ( 32 )
@@ -92,6 +110,16 @@ module xilinx_alveo
         .axi4s_in     ( __axis_h2c ),
         .axi4s_out    ( __axis_h2c__async )
     );
+
+    axi4s_fifo_async #(
+        .DEPTH        ( 32 )
+    ) i_axi4s_fifo_async__c2h (
+        .axi4s_in     ( __axis_c2h__async ),
+        .axi4s_out    ( __axis_c2h )
+    );
+
+    // Map between Alveo interfaces and QDMA interfaces
+    // -- H2C
     assign __axis_h2c__async.aclk = core_clk;
     assign __axis_h2c__async.aresetn = !core_srst;
 
@@ -101,17 +129,43 @@ module xilinx_alveo
     assign axis_h2c.tlast   = __axis_h2c__async.tlast;
     assign axis_h2c.tkeep   = __axis_h2c__async.tkeep;
     assign axis_h2c.tdata   = __axis_h2c__async.tdata;
-    assign axis_h2c.tid     = __axis_h2c__async.tid;
-    assign axis_h2c.tdest   = __axis_h2c__async.tdest;
-    assign axis_h2c.tuser   = __axis_h2c__async.tuser;
+
+    assign __axis_h2c_tid   = __axis_h2c__async.tid;
+    assign axis_h2c_tid.qid = __axis_h2c_tid.qid;
+    assign axis_h2c.tid     = axis_h2c_tid;
+
+    assign __axis_h2c_tdest      = __axis_h2c__async.tdest;
+    assign axis_h2c_tdest.unused = '0;
+    assign axis_h2c.tdest        = axis_h2c_tdest;
+
+    assign __axis_h2c_tuser   = __axis_h2c__async.tuser;
+    assign axis_h2c_tuser.err = __axis_h2c_tuser.err;
+    assign axis_h2c.tuser     = axis_h2c_tuser;
+
     assign __axis_h2c__async.tready = axis_h2c.tready;
 
-    axi4s_fifo_async #(
-        .DEPTH        ( 32 )
-    ) i_axi4s_fifo_async__c2h (
-        .axi4s_in     ( axis_c2h ),
-        .axi4s_out    ( __axis_c2h )
-    );
+    // -- C2H
+    assign __axis_c2h__async.aclk = core_clk;
+    assign __axis_c2h__async.aresetn = !core_srst;
+
+    assign __axis_c2h__async.tvalid = axis_c2h.tvalid;
+    assign __axis_c2h__async.tlast  = axis_c2h.tlast;
+    assign __axis_c2h__async.tkeep  = axis_c2h.tkeep;
+    assign __axis_c2h__async.tdata  = axis_c2h.tdata;
+
+    assign axis_c2h_tid          = axis_c2h.tid;
+    assign __axis_c2h_tid.qid    = axis_c2h_tid.qid;
+    assign __axis_c2h__async.tid = __axis_c2h_tid;
+
+    assign axis_c2h_tdest          = axis_c2h.tdest;
+    assign __axis_c2h_tdest.unused = 1'b0;
+    assign __axis_c2h__async.tdest = __axis_c2h_tdest;
+
+    assign axis_c2h_tuser          = axis_c2h.tuser;
+    assign __axis_c2h_tuser.err    = axis_c2h_tuser.err;
+    assign __axis_c2h__async.tuser = __axis_c2h_tuser;
+
+    assign axis_c2h.tready = __axis_c2h__async.tready;
 
     // =========================================================================
     // Clock/reset generators
@@ -125,14 +179,14 @@ module xilinx_alveo
 
     // Establish core clock domain
     assign core_clk = clk_333mhz;
-    
+
     sync_reset i_sync_reset__clk (
         .clk_in  ( axil_top.aclk ),
         .rst_in  ( axil_top.aresetn ),
         .clk_out ( core_clk ),
         .rst_out ( core_srst )
     );
-    
+
     // Export derived clocks to physical layer (hardware)
     assign alveo_hw_if.clk_100mhz = clk_100mhz;
     assign alveo_hw_if.clk_125mhz = clk_125mhz;
@@ -216,10 +270,30 @@ module xilinx_alveo
     // =========================================================================
     generate
         for (genvar g_cmac = 0; g_cmac < NUM_CMAC; g_cmac++) begin : g__cmac
-            axi4s_intf #(.DATA_BYTE_WID(CMAC_DATA_BYTE_WID), .TID_T(xilinx_cmac_pkg::axis_tid_t), .TUSER_T(xilinx_cmac_pkg::axis_tuser_t)) __axis_cmac_rx ();
-            axi4s_intf #(.DATA_BYTE_WID(CMAC_DATA_BYTE_WID), .TID_T(xilinx_cmac_pkg::axis_tid_t), .TUSER_T(xilinx_cmac_pkg::axis_tuser_t)) __axis_cmac_rx__async ();
-            axi4s_intf #(.DATA_BYTE_WID(CMAC_DATA_BYTE_WID), .TID_T(xilinx_cmac_pkg::axis_tid_t), .TUSER_T(xilinx_cmac_pkg::axis_tuser_t)) __axis_cmac_tx ();
+            // (Local) interfaces
+            axi4s_intf #(.DATA_BYTE_WID(CMAC_DATA_BYTE_WID), .TID_T(xilinx_cmac_pkg::axis_tid_t), .TDEST_T(xilinx_cmac_pkg::axis_tdest_t), .TUSER_T(xilinx_cmac_pkg::axis_tuser_t)) __axis_cmac_rx ();
+            axi4s_intf #(.DATA_BYTE_WID(CMAC_DATA_BYTE_WID), .TID_T(xilinx_cmac_pkg::axis_tid_t), .TDEST_T(xilinx_cmac_pkg::axis_tdest_t), .TUSER_T(xilinx_cmac_pkg::axis_tuser_t)) __axis_cmac_rx__async ();
+            axi4s_intf #(.DATA_BYTE_WID(CMAC_DATA_BYTE_WID), .TID_T(xilinx_cmac_pkg::axis_tid_t), .TDEST_T(xilinx_cmac_pkg::axis_tdest_t), .TUSER_T(xilinx_cmac_pkg::axis_tuser_t)) __axis_cmac_tx__async ();
+            axi4s_intf #(.DATA_BYTE_WID(CMAC_DATA_BYTE_WID), .TID_T(xilinx_cmac_pkg::axis_tid_t), .TDEST_T(xilinx_cmac_pkg::axis_tdest_t), .TUSER_T(xilinx_cmac_pkg::axis_tuser_t)) __axis_cmac_tx ();
 
+            // (Local) signals
+            xilinx_cmac_pkg::axis_tid_t   __axis_cmac_rx_tid;
+            xilinx_cmac_pkg::axis_tdest_t __axis_cmac_rx_tdest;
+            xilinx_cmac_pkg::axis_tuser_t __axis_cmac_rx_tuser;
+
+            cmac_axis_tid_t   axis_cmac_rx_tid;
+            cmac_axis_tdest_t axis_cmac_rx_tdest;
+            cmac_axis_tuser_t axis_cmac_rx_tuser;
+
+            cmac_axis_tid_t   axis_cmac_tx_tid;
+            cmac_axis_tdest_t axis_cmac_tx_tdest;
+            cmac_axis_tuser_t axis_cmac_tx_tuser;
+
+            xilinx_cmac_pkg::axis_tid_t   __axis_cmac_tx_tid;
+            xilinx_cmac_pkg::axis_tdest_t __axis_cmac_tx_tdest;
+            xilinx_cmac_pkg::axis_tuser_t __axis_cmac_tx_tuser;
+
+            // CMAC instantatiation
             xilinx_alveo_cmac #(
                 .PORT_ID ( g_cmac )
             ) i_xilinx_alveo_cmac (
@@ -243,6 +317,16 @@ module xilinx_alveo
                 .axi4s_in     ( __axis_cmac_rx ),
                 .axi4s_out    ( __axis_cmac_rx__async )
             );
+
+            axi4s_fifo_async #(
+                .DEPTH        ( 32 )
+            ) i_axi4s_fifo_async__cmac_tx (
+                .axi4s_in     ( __axis_cmac_tx__async ),
+                .axi4s_out    ( __axis_cmac_tx )
+            );
+
+            // Map between Alveo interfaces and QDMA interfaces
+            // -- CMAC Rx
             assign __axis_cmac_rx__async.aclk = core_clk;
             assign __axis_cmac_rx__async.aresetn = !core_srst;
 
@@ -252,21 +336,45 @@ module xilinx_alveo
             assign axis_cmac_rx[g_cmac].tlast   = __axis_cmac_rx__async.tlast;
             assign axis_cmac_rx[g_cmac].tkeep   = __axis_cmac_rx__async.tkeep;
             assign axis_cmac_rx[g_cmac].tdata   = __axis_cmac_rx__async.tdata;
-            assign axis_cmac_rx[g_cmac].tid     = __axis_cmac_rx__async.tid;
-            assign axis_cmac_rx[g_cmac].tdest   = __axis_cmac_rx__async.tdest;
-            assign axis_cmac_rx[g_cmac].tuser   = __axis_cmac_rx__async.tuser;
+
+            assign __axis_cmac_rx_tid       = __axis_cmac_rx__async.tid;
+            assign axis_cmac_rx_tid.unused  = 1'b0;
+            assign axis_cmac_rx[g_cmac].tid = axis_cmac_rx_tid;
+
+            assign __axis_cmac_rx_tdest       = __axis_cmac_rx__async.tdest;
+            assign axis_cmac_rx_tdest.unused  = 1'b0;
+            assign axis_cmac_rx[g_cmac].tdest = axis_cmac_rx_tdest;
+
+            assign __axis_cmac_rx_tuser       = __axis_cmac_rx__async.tuser;
+            assign axis_cmac_rx_tuser.err     = __axis_cmac_rx_tuser.err;
+            assign axis_cmac_rx[g_cmac].tuser = axis_cmac_rx_tuser;
+
             assign __axis_cmac_rx__async.tready = axis_cmac_rx[g_cmac].tready;
 
-            axi4s_fifo_async #(
-                .DEPTH        ( 32 )
-            ) i_axi4s_fifo_async__cmac_tx (
-                .axi4s_in     ( axis_cmac_tx[g_cmac] ),
-                .axi4s_out    ( __axis_cmac_tx )
-            );
+            // -- CMAC Tx
+            assign __axis_cmac_tx__async.aclk    = core_clk;
+            assign __axis_cmac_tx__async.aresetn = !core_srst;
+
+            assign __axis_cmac_tx__async.tvalid = axis_cmac_tx[g_cmac].tvalid;
+            assign __axis_cmac_tx__async.tlast  = axis_cmac_tx[g_cmac].tlast;
+            assign __axis_cmac_tx__async.tkeep  = axis_cmac_tx[g_cmac].tkeep;
+            assign __axis_cmac_tx__async.tdata  = axis_cmac_tx[g_cmac].tdata;
+
+            assign axis_cmac_tx_tid           = axis_cmac_tx[g_cmac].tid;
+            assign __axis_cmac_tx_tid.unused  = 1'b0;
+            assign __axis_cmac_tx__async.tid  = __axis_cmac_tx_tid;
+
+            assign axis_cmac_tx_tdest          = axis_cmac_tx[g_cmac].tdest;
+            assign __axis_cmac_tx_tdest.unused = 1'b0;
+            assign __axis_cmac_tx__async.tdest = __axis_cmac_tx_tdest;
+
+            assign axis_cmac_tx_tuser          = axis_cmac_tx[g_cmac].tuser;
+            assign __axis_cmac_tx_tuser.err    = axis_cmac_tx_tuser.err;
+            assign __axis_cmac_tx__async.tuser = __axis_cmac_tx_tuser;
+
+            assign axis_cmac_tx[g_cmac].tready = __axis_cmac_tx__async.tready;
+
         end : g__cmac
-        for (genvar g_cmac = NUM_CMAC; g_cmac < NUM_CMAC_REGMAP; g_cmac++) begin : g__cmac_reg_tieoff
-            axi4l_intf_peripheral_term (.axi4l_if (axil_cmac[g_cmac]));
-        end : g__cmac_reg_tieoff
     endgenerate
 
     // =========================================================================
