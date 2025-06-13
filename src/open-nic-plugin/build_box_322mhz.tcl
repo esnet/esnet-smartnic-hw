0d3b4d11925c0e4d9e8b180ee0e11be32d362472
Jonathan Sewter <jsewter@es.net>
2025-06-12 21:05:01 -0400

    open-nic-plugin: find tool version from XILINX_VIVADO env var.

--- src/open-nic-plugin/build_box_322mhz.tcl
+++ src/open-nic-plugin/build_box_322mhz.tcl
@@ -1,12 +1,14 @@
 set curdir [pwd]
 cd ../smartnic/build/
 
+set __XILINX_VERSION [regexp -inline {[0-9]{4}\.[0-9]} $env(XILINX_VIVADO)]
+
 # read design sources
-source ../../../.out/${board}/[file tail $env(XILINX_VIVADO)]/smartnic/build/synth/sources.tcl
+source ../../../.out/${board}/${__XILINX_VERSION}/smartnic/build/synth/sources.tcl
 read_checkpoint -cell box_322mhz_inst/smartnic/smartnic_app $env(APP_ROOT)/app_if/smartnic_app.dcp
 
 # read constraints
-source ../../../.out/${board}/[file tail $env(XILINX_VIVADO)]/smartnic/build/synth/constraints.tcl
+source ../../../.out/${board}/${__XILINX_VERSION}/smartnic/build/synth/constraints.tcl
 
 read_xdc constraints/${board}/timing.xdc
 read_xdc constraints/${board}/place.xdc
