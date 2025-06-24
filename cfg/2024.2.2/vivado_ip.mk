a1c00b94db5f97fae64b09c444e51a4dad772d60
Jonathan Sewter <jsewter@es.net>
2025-06-24 09:50:09 -0400

    cfg: stage IP version support for Vivado 2024.2, 2024.2.2 and 2025.1.

---
+++ cfg/2024.2.2/vivado_ip.mk
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------------
+# Vivado IP configuration
+#
+# List of version-specific IP cores used in the project
+# Format is [Vivado IP def spec]=[IP core revision]
+# e.g. xilinx.com:ip:cmac_usplus:3.1=1
+# ------------------------------------------------------------------
+VIVADO_IP = \
+    xilinx.com:ip:cmac_usplus:3.1=21 \
+    xilinx.com:ip:gtwizard_ultrascale:1.7=19 \
+    xilinx.com:ip:ecc:2.0=17 \
+    xilinx.com:ip:qdma:5.0=14 \
+    xilinx.com:ip:vitis_net_p4:2.2=0 \
+    xilinx.com:ip:cam:3.1=0 \
+    xilinx.com:ip:cdcam:1.2=0 \
+    xilinx.com:ip:axi_register_slice:2.1=34 \
+    xilinx.com:ip:axis_register_slice:1.1=34
+
---
+++ cfg/2025.1/vivado_ip.mk
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------------
+# Vivado IP configuration
+#
+# List of version-specific IP cores used in the project
+# Format is [Vivado IP def spec]=[IP core revision]
+# e.g. xilinx.com:ip:cmac_usplus:3.1=1
+# ------------------------------------------------------------------
+VIVADO_IP = \
+    xilinx.com:ip:cmac_usplus:3.1=22 \
+    xilinx.com:ip:gtwizard_ultrascale:1.7=20 \
+    xilinx.com:ip:ecc:2.0=18 \
+    xilinx.com:ip:qdma:5.1=0 \
+    xilinx.com:ip:vitis_net_p4:2.3=0 \
+    xilinx.com:ip:cam:4.0=0 \
+    xilinx.com:ip:cdcam:1.3=0 \
+    xilinx.com:ip:axi_register_slice:2.1=35 \
+    xilinx.com:ip:axis_register_slice:1.1=35
+
