9bcb92a1696bb028df9ce053a14bd968a61da077
Jonathan Sewter <jsewter@es.net>
2025-10-15 13:09:43 -0400

    xilinx.hbm: remove control proxy register interface.

--- src/xilinx/hbm/rtl/src/xilinx_hbm_stack_ctrl.sv
+++ src/xilinx/hbm/rtl/src/xilinx_hbm_stack_ctrl.sv
@@ -12,9 +12,6 @@ module xilinx_hbm_stack_ctrl
     // AXI-L control interface
     axi4l_intf.peripheral axil_if,
 
-    // AXI3 memory control channel interface
-    axi3_intf.controller  control_proxy_axi_if,
-    
     // APB (management) interface
     input logic           apb_clk,
     apb_intf.controller   apb_if,
@@ -57,7 +54,6 @@ module xilinx_hbm_stack_ctrl
     axi4l_intf hbm_axil_if__clk ();
     axi4l_intf hbm_apb_proxy_axil_if ();
     axi4l_intf hbm_apb_proxy_axil_if__apb_clk ();
-    axi4l_intf control_proxy_axil_if ();
 
     apb_intf hbm_channel_apb_if ();
 
@@ -70,8 +66,7 @@ module xilinx_hbm_stack_ctrl
     xilinx_hbm_decoder i_xilinx_hbm_decoder (
         .axil_if                      ( axil_if ),
         .xilinx_hbm_axil_if           ( hbm_axil_if ),
-        .xilinx_hbm_apb_proxy_axil_if ( hbm_apb_proxy_axil_if ),
-        .control_proxy_axil_if        ( control_proxy_axil_if )
+        .xilinx_hbm_apb_proxy_axil_if ( hbm_apb_proxy_axil_if )
     );
 
     // CDC
@@ -135,49 +130,6 @@ module xilinx_hbm_stack_ctrl
     axi4l_apb_proxy i_axi4l_apb_proxy (
         .axi4l_if ( hbm_apb_proxy_axil_if__apb_clk ),
         .apb_if   ( apb_if )
-    );
-
-    // -----------------------------
-    // AXI-3 control proxy
-    // -----------------------------
-    // (Local) parameters
-    localparam int MEM_ADDR_WID = AXI_ADDR_WID - $clog2(AXI_DATA_BYTE_WID); // Memory interface uses row addressing
-
-    // (Local) Interfaces
-    mem_intf #(.ADDR_WID(MEM_ADDR_WID), .DATA_WID(AXI_DATA_WID)) __mem_if (.clk(clk));
-    mem_wr_intf #(.ADDR_WID(MEM_ADDR_WID), .DATA_WID(AXI_DATA_WID)) __mem_wr_if (.clk(clk));
-    mem_rd_intf #(.ADDR_WID(MEM_ADDR_WID), .DATA_WID(AXI_DATA_WID)) __mem_rd_if (.clk(clk));
-
-    // Memory register proxy
-    // (drive AXI-3 memory interface from register proxy controller)
-    mem_proxy       #(
-        .ACCESS_TYPE ( mem_pkg::ACCESS_READ_WRITE ),
-        .MEM_TYPE    ( mem_pkg::MEM_TYPE_HBM )
-    ) i_mem_proxy__control (
-        .clk         ( clk ),
-        .srst        ( local_srst ),
-        .init_done   ( ),
-        .axil_if     ( control_proxy_axil_if ),
-        .mem_if      ( __mem_if )
-    );
-
-    mem_sp_to_sdp_adapter i_mem_sp_to_sdp_adapter__hbm (
-        .mem_if ( __mem_if ),
-        .mem_wr_if ( __mem_wr_if ),
-        .mem_rd_if ( __mem_rd_if )
-    );
-
-    axi3_from_mem_adapter #(
-        .SIZE ( axi3_pkg::SIZE_32BYTES ),
-        .WR_TIMEOUT ( 0 ),
-        .RD_TIMEOUT ( 0 )
-    ) i_axi3_from_mem_adapter (
-        .clk           ( clk ),
-        .srst          ( local_srst ),
-        .init_done     ( ),
-        .mem_wr_if     ( __mem_wr_if ),
-        .mem_rd_if     ( __mem_rd_if ),
-        .axi3_if       ( control_proxy_axi_if )
     );
 
 endmodule : xilinx_hbm_stack_ctrl
