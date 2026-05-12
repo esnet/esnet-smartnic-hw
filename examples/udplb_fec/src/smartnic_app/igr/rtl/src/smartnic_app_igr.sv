230fdc884ba4dd51ef6a4bff8fbf51fc9557aed3
Peter Bengough <pbengough@es.net>
2026-05-12 14:36:02 -0400

    (examples:udplb_fec) Adds symbol-to-bit column transposition logic following rs decoder.

--- examples/udplb_fec/src/smartnic_app/igr/rtl/src/smartnic_app_igr.sv
+++ examples/udplb_fec/src/smartnic_app/igr/rtl/src/smartnic_app_igr.sv
@@ -72,45 +72,35 @@ module smartnic_app_igr
 
     localparam int DATA_WID = DATA_BYTE_WID*8;
 
-    rs_acc_intf #(.DATA_WID(DATA_WID)) col_in  [NUM_PORTS] (.clk(core_clk));
-    rs_acc_intf #(.DATA_WID(DATA_WID)) col_out [NUM_PORTS] (.clk(core_clk));
+    rs_acc_intf #(.DATA_WID(DATA_WID)) b2s_in  [NUM_PORTS] (.clk(core_clk));
+    rs_acc_intf #(.DATA_WID(DATA_WID)) b2s_out [NUM_PORTS] (.clk(core_clk));
     rs_acc_intf #(.DATA_WID(DATA_WID)) inj_out [NUM_PORTS] (.clk(core_clk));
     rs_acc_intf #(.DATA_WID(DATA_WID)) frm_out [NUM_PORTS] (.clk(core_clk));
     rs_acc_intf #(.DATA_WID(DATA_WID)) dec_out [NUM_PORTS] (.clk(core_clk));
-
-    /*
-    axi4s_intf  #(.DATA_BYTE_WID(DATA_BYTE_WID), .TUSER_WID(TUSER_WID), .TID_WID(TID_WID),
-                  .TDEST_WID(TDEST_WID))  axi4s_dec_in (.aclk(core_clk));
-
-    assign axi4s_dec_in.tdata  = inj_out[0].data;
-    assign axi4s_dec_in.tkeep  = {'0, inj_out[0].meta.fec_blk_size};
-    assign axi4s_dec_in.tvalid = inj_out[0].valid;
-    assign axi4s_dec_in.tready = inj_out[0].ready;
-    */
-    // xilinx_axi4s_ila #(.PIPE_STAGES(2)) xilinx_axi4s_dec_in (.axis_in(axi4s_dec_in));
+    rs_acc_intf #(.DATA_WID(DATA_WID)) s2b_out [NUM_PORTS] (.clk(core_clk));
 
     generate for (genvar i = 0; i < NUM_PORTS; i += 1) begin
-        assign col_in[i].data     = fec_in[i].tdata;
-        assign col_in[i].valid    = fec_in[i].tvalid;
+        assign b2s_in[i].data     = fec_in[i].tdata;
+        assign b2s_in[i].valid    = fec_in[i].tvalid;
 
-        assign fec_in[i].tready = col_in[i].ready;
+        assign fec_in[i].tready = b2s_in[i].ready;
 
         fec_col_transpose #(
             .DATA_WID      (DATA_WID),
             .COL_WID       (SYM_SIZE),
             .MODE          (BIT_TO_SYM)
-        ) fec_sym_to_bit_0 (
+        ) fec_bit_to_sym_0 (
             .clk           (core_clk),
             .srst          (core_srst),
-            .data_in       (col_in[i]),
-            .data_out      (col_out[i])
+            .data_in       (b2s_in[i]),
+            .data_out      (b2s_out[i])
         );
 
         rs_acc_err_inj #(.DATA_WID(DATA_WID)) rs_acc_err_inj_0 (
             .clk           (core_clk),
             .srst          (core_srst),
             .err_loc_vec   (RS_ERR_LOC_LUT[smartnic_app_igr_regs.app_igr_config.err_loc_inj]),
-            .data_in       (col_out[i]),
+            .data_in       (b2s_out[i]),
             .data_out      (inj_out[i])
         );
 
@@ -131,17 +121,28 @@ module smartnic_app_igr
             .data_out       (dec_out[i])
         );
 
-        always_comb begin
-            for (int j = 0; j < DATA_BYTE_WID; j++) _axi4s_c2h[i].tkeep[j] = dec_out[i].meta.keep > j ? 1'b1 : 1'b0;
+        fec_col_transpose #(
+            .DATA_WID      (DATA_WID),
+            .COL_WID       (SYM_SIZE),
+            .MODE          (SYM_TO_BIT)
+        ) fec_sym_to_bit_0 (
+            .clk           (core_clk),
+            .srst          (core_srst),
+            .data_in       (dec_out[i]),
+            .data_out      (s2b_out[i])
+        );
 
-            _axi4s_c2h[i].tdata  = dec_out[i].data;
-            _axi4s_c2h[i].tvalid = dec_out[i].valid;
+        always_comb begin
+            for (int j = 0; j < DATA_BYTE_WID; j++) _axi4s_c2h[i].tkeep[j] = s2b_out[i].meta.keep > j ? 1'b1 : 1'b0;
+
+            _axi4s_c2h[i].tdata  = s2b_out[i].data;
+            _axi4s_c2h[i].tvalid = s2b_out[i].valid && (s2b_out[i].meta.keep != 0);
             _axi4s_c2h[i].tid    = '0;  // tie to 0
             _axi4s_c2h[i].tdest  = '0;  // tie to 0
             _axi4s_c2h[i].tuser  = '0;  // TODO: plumb tuser metadata for egr q selection.
-            _axi4s_c2h[i].tlast  = dec_out[i].meta.eos;
+            _axi4s_c2h[i].tlast  = s2b_out[i].meta.eos;
 
-            dec_out[i].ready = _axi4s_c2h[i].tready;
+            s2b_out[i].ready = _axi4s_c2h[i].tready;
         end
 
         axi4s_full_pipe axis4s_full_pipe_inst (.srst, .from_tx(_axi4s_c2h[i]), .to_rx(axi4s_c2h[i]));
